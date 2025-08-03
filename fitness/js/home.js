ea23f677b8c466b1ab7031a5df91a78a1a2cd258
bhagyashree-prusty <bhagya4shree25@gmail.com>
2025-08-03 22:36:44 +0530

    Add files via upload

---
+++ fitness/js/home.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+    
+    ctaButton.addEventListener('click', function() {
+        alert('Welcome to AthleticPro! The system will guide you through team setup.');
+    });
+});
